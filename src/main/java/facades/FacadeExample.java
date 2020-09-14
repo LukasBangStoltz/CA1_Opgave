6c3e86e39358464e5318af92271d54f2607b223a
Phillip Andersen <phillip_andersen@hotmail.com>
2020-09-14 16:26:57 +0200

    memberstabel opgaven er godt i gang, endpoint, facade metode, samt DTOklassen er lavet. Næste step er testing

--- src/main/java/facades/FacadeExample.java
+++ src/main/java/facades/FacadeExample.java
@@ -1,6 +1,8 @@
 package facades;
 
-import entities.RenameMe;
+import DTO.MemberDTO;
+import entities.Member;
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -14,13 +16,13 @@ public class FacadeExample {
 
     private static FacadeExample instance;
     private static EntityManagerFactory emf;
-    
+
     //Private Constructor to ensure Singleton
-    private FacadeExample() {}
-    
-    
+    private FacadeExample() {
+    }
+
     /**
-     * 
+     *
      * @param _emf
      * @return an instance of this facade class.
      */
@@ -35,17 +37,29 @@ public class FacadeExample {
     private EntityManager getEntityManager() {
         return emf.createEntityManager();
     }
-    
-    //TODO Remove/Change this before use
-    public long getRenameMeCount(){
+
+    public List<MemberDTO> getAllMembers() {
         EntityManager em = emf.createEntityManager();
-        try{
-            long renameMeCount = (long)em.createQuery("SELECT COUNT(r) FROM RenameMe r").getSingleResult();
-            return renameMeCount;
-        }finally{  
+
+        List<Member> allMembers = null;
+        List<MemberDTO> allMembersDTO = new ArrayList();
+
+        try {
+            allMembers = em.createQuery("SELECT m FROM Member m", Member.class).getResultList();
+
+            allMembers.forEach((Member member) -> {
+
+                allMembersDTO.add(new MemberDTO(member));
+
+            });
+
+            return allMembersDTO;
+        } finally {
             em.close();
         }
-        
+
     }
 
 }
+
+
