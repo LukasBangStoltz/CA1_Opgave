4877eeef8f0240518ec13b9a194c4abec1d55a52
Phillip Andersen <phillip_andersen@hotmail.com>
2020-09-15 13:10:36 +0200

    Den er der næsten, små problemer med databasen, sebastian tager over, for at teste

---
+++ src/main/webapp/javascript/FetchDataMembers.js
@@ -0,0 +1,32 @@
+
+
+
+
+function fetchAllMembers() {
+
+    //let url = "https://https://kodekongen.dk/CA1_Opgave/api/groupmembers/all";
+    let url = "http://localhost:8080/jpareststarter/api/groupmembers/all"
+    fetch(url)
+            .then(res => res.json())
+            .then(date => {
+                let allMembers = document.getElementById("tbody");
+                let result = '';
+                data.forEach((x) => {
+                    result += getTableRow(x)
+                });
+                allMembers.innerHTML = result;
+
+
+            });
+
+
+    function getTableRow(member) {
+
+        return `<tr><td>${member.name}</td><td>${member.studentId}</td><td>${member.favoriteSeries}</td></tr>`
+    }
+
+
+
+
+
+}
