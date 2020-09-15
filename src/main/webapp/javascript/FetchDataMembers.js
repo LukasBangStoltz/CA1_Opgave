926dd113000f3457997aeba41c5433028d06637e
Sebastian Godsk Hansen <cph-sh497@cphbusiness.dk>
2020-09-15 16:01:19 +0200

    Lavede fetch metode og knap til knap på index side

--- src/main/webapp/javascript/FetchDataMembers.js
+++ src/main/webapp/javascript/FetchDataMembers.js
@@ -1,32 +1,36 @@
 
 
+/* global then */
 
+let getFetchButton = document.getElementById("fetchButton");
+
+getFetchButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    fetchAllMembers();
+});
 
 function fetchAllMembers() {
-
     //let url = "https://https://kodekongen.dk/CA1_Opgave/api/groupmembers/all";
-    let url = "http://localhost:8080/jpareststarter/api/groupmembers/all"
+    let url = "http://localhost:8080/jpareststarter/api/groupmembers/all/";
     fetch(url)
             .then(res => res.json())
-            .then(date => {
+            .then(data => {
                 let allMembers = document.getElementById("tbody");
                 let result = '';
                 data.forEach((x) => {
                     result += getTableRow(x)
                 });
-                allMembers.innerHTML = result;
-
-
+                allMembers.innerHTML = result
             });
+}
+/*
+ function getTableHeader() {
+ return '<thead><th>Name</th><th>StudentId</th><th>Favorite Series</th></thead>';
+ }
+ * 
+ */
 
 
-    function getTableRow(member) {
-
-        return `<tr><td>${member.name}</td><td>${member.studentId}</td><td>${member.favoriteSeries}</td></tr>`
-    }
-
-
-
-
-
+function getTableRow(member) {
+    return `<tr><td>${member.name}</td><td>${member.studentId}</td><td>${member.favoriteSeries}</td></tr>`;
 }
